96806d354f5554e45ae90d5c402d8304187f408a
idreamshen <idream.shen@gmail.com>
2026-01-25 10:43:24 +0800

    chore: Add Nix flake configuration and update .gitignore

---
+++ .nix/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Nix flake for hass-ssd1306 development";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.python313
+              pkgs.python313Packages.pillow
+            ];
+          };
+        }
+      );
+    };
+}
